24766fb67820b830b81b0c66b295d9252e70275a
Mkim <sp3dng9@gmail.com>
2019-01-04 22:15:06 -0800

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Double Double", true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Burger Royale with Cheese",true);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Quarter Pounder", true);
